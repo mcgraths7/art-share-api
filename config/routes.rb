7f299fbb9a37aafc693f97641902df95be26a8bd
mcgraths7 <mcgrath.steven7@gmail.com>
2020-07-14 12:07:19 -0400

    created comments table, model w/ associations, and controller/routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   resources :users do
     resources :artworks, only: [:index]
   end
+  resources :users do
+    resources :comments, only: [:index]
+  end
+  resources :artworks do
+    resources :comments, only: [:index]
+  end
   resources :artworks, only: [:create, :show, :update, :destroy]
   resources :artwork_shares, only: [:create, :destroy]
+  resources :comments, only: [:create, :destroy]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_11_125630) do
+ActiveRecord::Schema.define(version: 2020_07_14_142550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 2020_07_11_125630) do
     t.datetime "updated_at", null: false
     t.index ["artist_id"], name: "index_artworks_on_artist_id"
     t.index ["title"], name: "index_artworks_on_title"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "artwork_id"
+    t.string "body"
+    t.index ["artwork_id"], name: "index_comments_on_artwork_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
